0ded045c11ec9019199be30cdfb4d0a17f040c98
Emily Thomas <emily.thomas14@accesscfa.com>
2026-09-17 14:14:56 -0400

    adding a few todos in comments and a few base functions

--- server.py
+++ server.py
@@ -7,3 +7,11 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Book Depot open at port {PORT}")
     httpd.serve_forever()
+
+    '''
+    List of todos: 
+    1. Add listeners for adding cases, shelves, books
+    2. Add lookup for books
+    3. Add sqlite backend
+    4. Add startup command
+    '''
--- util/dataModels/DataModels.py
+++ util/dataModels/DataModels.py
@@ -12,16 +12,34 @@ class Bookcase(object):
     def generate_shelf(self, **kwargs):
         shelf = Shelf(bookcase=self, **kwargs)
 
+    def name_case(self, name: str):
+        self.name = name
+
+    def update_sort(self, sorting_method: str):
+        self.sorting_method = sorting_method
+
+    def update_genre(self, genre: str):
+        self.genre = genre
+
 @dataclass
 class Shelf(object):
-    number: int
+    number: int | None
     id: uuid.UUID | uuid.uuid4()
     bookcase: Bookcase | None
     sorting_method: str | None
     genre: str | None
 
+    def update_number(self, number: int):
+        self.number = number
+
     def update_bookcase(self, bookcase: Bookcase):
         self.bookcase = bookcase
+
+    def update_sorting_method(self, sorting_method: str):
+        self.sorting_method = sorting_method
+
+    def update_genre(self, genre: str):
+        self.genre = genre
 
 @dataclass
 class Book:
@@ -43,3 +61,7 @@ class Book:
 
     def update_shelf(self, shelf: Shelf):
         self.shelf = shelf
+
+    def update_attributes(self, attributes: dict):
+        # In progress
+        self.author = attributes["author"]
