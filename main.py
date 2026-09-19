d7fddbb72986133627236985d8373024b5f7c894
Emily Thomas <emily.thomas14@accesscfa.com>
2026-09-19 09:58:36 -0400

    starting to build out main

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from util.database.database import Database
+from util.database.repo import DatabaseRepo
+
 def __main__():
-    
+
+    try:
+        database = Database()
+
+        Database.database_init()
+
+    except Exception as error:
+        print(error)
 
     return
 
